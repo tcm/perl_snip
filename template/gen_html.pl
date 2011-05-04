1d4736cdf57d3de746f8683ef4bd26f675625254
Juergen Bachsteffel <juergen@pass-ag.com>
2011-05-04 18:35:37 +0200

    Kleine Änderungen.

--- template/gen_html.pl
+++ template/gen_html.pl
@@ -4,11 +4,18 @@ use strict;
 use FindBin '$Bin';
 use Template;
 
+my   $vars = {
+           
+                 url =>"ab", 
+                 title=>"cd"
+            
+        
+      };
 
 my $tt = Template->new({
     INCLUDE_PATH => "$Bin/tt",
     INTERPOLATE  => 1,
 }) || die "$Template::ERROR\n";
 
-$tt->process('html.tt')
+$tt->process('html.tt',$vars)
     || die $tt->error(), "\n";
