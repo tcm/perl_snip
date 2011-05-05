3a2fcc4fb34f5ddfd6125f5899ef23f4bf3b96e3
Juergen Bachsteffel <juergen@pass-ag.com>
2011-05-05 12:41:47 +0200

    Jetzt funktionierts. :-)

--- template/gen_html.pl
+++ template/gen_html.pl
@@ -1,21 +1,49 @@
 #!/usr/bin/perl
 
 use strict;
+use  warnings;
+
 use FindBin '$Bin';
 use Template;
+use CGI;
 
-my   $vars = {
-           
-                 url =>"ab", 
-                 title=>"cd"
-            
-        
-      };
+$| = 1;
+print "Content-type: text/html\n\n";
 
-my $tt = Template->new({
+my $file = 'html.tt';
+my $vars = {
+    'version'  => 3.14,
+    'days'     => [ qw( mon tue wed thu fri sat sun ) ],
+    'worklist' => \&get_user_projects,
+    'cgi'      => CGI->new(),
+    'me'       => {
+        'id'     => 'hs',
+        'name'   => 'Homer Simpson',
+    },
+    'email' => 'hs@web.de'
+};
+
+
+sub get_user_projects {
+    my $user = shift;
+    my @projects = ( 
+       { 
+	  url       => "http://web.de", 
+	  name      => "abc", 
+       },
+       {
+	   url     => "http://yahoo.de",
+	   name    => "cde"
+       }
+    );
+    return \@projects;
+}
+
+my $template = Template->new({
     INCLUDE_PATH => "$Bin/tt",
-    INTERPOLATE  => 1,
-}) || die "$Template::ERROR\n";
+    INTERPOLATE  => 1
+});
 
-$tt->process('html.tt',$vars)
-    || die $tt->error(), "\n";
+$template->process($file, $vars)
+    || die $template->error();
+
