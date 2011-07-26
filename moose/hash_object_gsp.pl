a4cba88f9df8ff533cfbdf32298f972623b45114
Juergen Bachsteffel <juergen@pass-ag.com>
2011-07-26 20:13:55 +0200

    Moose-Goodies. :-)

--- moose/hash_object_gsp.pl
+++ moose/hash_object_gsp.pl
@@ -1,10 +1,17 @@
 #!/usr/bin/perl
 
+# Objekte mit Moose und den eingebauten
+# Goodies für perl-Datentypen. :-)
+#
+# (jb) 2011
+
+
 use Data::Dumper;
 
-package Geldspeicher;
+package Speicher;
   use Moose;
 
+# Vertige Klasse fuer den Datentyp 'Hash'.
   has 'mapping' => (
       traits  => ['Hash'],
       is      => 'rw',
@@ -18,16 +25,54 @@ package Geldspeicher;
       },
   );
 
-  my $obj = Geldspeicher->new;
-  $obj->set_mapping(0.1, 4); 
-  $obj->set_mapping(0.2, 5); 
-  $obj->set_mapping(0.5, 6); 
+# Rein.
+sub fuellen {
+      my ( $self, $id, $menge ) = @_;
+
+      if ( $self->exists_in_mapping($id) ) { 
+      		$self->set_mapping( $id, $self->get_mapping($id) + $menge );
+      } else {
+		print "Id:$id nicht vorhanden.\n";
+      }
+}
+
+# Raus.
+sub leeren {
+	my ( $self, $id, $menge ) = @_;
+       
+	if ( $self->exists_in_mapping($id) ) { 
+		$self->set_mapping( $id, $self->get_mapping($id) - $menge );
+	} else {  
+		print "Id:$id nicht vorhanden.\n";
+	}
+}
+
+
+# Klasse kann nicht mehr veraendert werden.
+__PACKAGE__->meta->make_immutable;
+
+
+  my $obj = Speicher->new;
+  $obj->set_mapping(0.1, 5); 
+  $obj->set_mapping(0.2, 6); 
+  $obj->set_mapping(0.5, 7); 
 
 
 
   # prints 5
   print $obj->get_mapping(0.1)."\n" if $obj->exists_in_mapping(0.1);
+  print "\n";
+
+  # prints 8
+  $obj->fuellen(0.1, 3); 
+  print $obj->get_mapping(0.1)."\n" if $obj->exists_in_mapping(0.1);
   print "\n"; 
   
+ # prints 1
+  $obj->leeren(0.1, 7);
+  print $obj->get_mapping(0.1)."\n" if $obj->exists_in_mapping(0.1);
+  print "\n";
+ 
   print join ', ', $obj->ids_in_mapping;
+
   print "\n";
