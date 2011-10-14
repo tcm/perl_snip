1fab0580c75b6785c744b2ab34e79e27004cb8a1
Juergen Bachsteffel <juergen@pass-ag.com>
2011-10-14 20:31:33 +0200

    Kosmetik.

--- hinventory/check_xml.pl
+++ hinventory/check_xml.pl
@@ -17,20 +17,23 @@ my %opts;
 
 getopts('i:h', \%opts);
 
-if (defined $opts{h}) {
-	print "check_xml.pl -i [-h]\n\n";
-	print "-h Hilfe\n";
-	print "-i Eingabedatei\n";
-	exit 1;
+if (defined $opts{h}) 
+{
+print "check_xml.pl -i [-h]\n\n";
+print "-h Hilfe\n";
+print "-i Eingabedatei\n";
+exit 0;
 }
 
-if (defined $opts{i}) {
-	$file_name = $opts{i};
-
-} else {
-	print "Eingabedatei angeben.\n";
-	print "check_xml.pl -i foo.xml\n";
-	exit 0;
+if (defined $opts{i}) 
+{
+$file_name = $opts{i};
+} 
+else 
+{
+print "Eingabedatei angeben.\n";
+print "check_xml.pl -i foo.xml\n";
+exit 1;
 }
 
 #########################################
@@ -38,7 +41,7 @@ if (defined $opts{i}) {
 #########################################
 my $parser = XML::LibXML->new();
 
-eval {my $doc = $parser->parse_file( $file_name ); };
+eval { my $doc = $parser->parse_file( $file_name ); };
 
 print "$@"."\n" if( $@ );     # Nein? Dann Fehlermeldung ausgeben.
-1;
+exit 0;
--- hinventory/gen_html_report_via_xslt.pl
+++ hinventory/gen_html_report_via_xslt.pl
@@ -19,20 +19,23 @@ my %opts;
 
 getopts('i:h', \%opts);
 
-if (defined $opts{h}) {
-	print "gen_html_report_via_xslt.pl -i [-h]\n\n";
-	print "-h Hilfe\n";
-	print "-i Eingabedatei\n";
-	exit 1;
+if (defined $opts{h}) 
+{
+print "gen_html_report_via_xslt.pl -i [-h]\n\n";
+print "-h Hilfe\n";
+print "-i Eingabedatei\n";
+exit 0;
 }
 
-if (defined $opts{i}) {
-	$xmlfile = $opts{i};
-
-} else {
-	print "Eingabedatei angeben.\n";
-	print "gen_html_report_via_xslt.pl -i foo.xml\n";
-	exit 0;
+if (defined $opts{i}) 
+{
+$xmlfile = $opts{i};
+} 
+else 
+{
+print "Eingabedatei angeben.\n";
+print "gen_html_report_via_xslt.pl -i foo.xml\n";
+exit 1;
 }
 
 
@@ -41,25 +44,27 @@ my $xslt = eval { XML::XSLT->new($xslfile, warnings => 1, debug
 => 0) };
 
 # error handling here ...
-if ($@) {
-        die("Sorry, Could not create an instance of the XSL
-Processor using $xslfile.\n");
+if ($@) 
+{
+die("Sorry, Could not create an instance of the XSL Processor using $xslfile.\n");
 }
 
 # load XML
 eval { $xslt->open_xml($xmlfile) };
 
 # .. here ...
-if ($@) {
-        die("Sorry, Could not load XML file, $xmlfile.\n");
+if ($@) 
+{
+die("Sorry, Could not load XML file, $xmlfile.\n");
 }
 
 # transform XML file
 eval { $xslt->process(debug => 0) };
 
 # .. and here.
-if ($@) {
-        die("Sorry, Could not transform XML file, $xmlfile.\n");
+if ($@) 
+{
+die("Sorry, Could not transform XML file, $xmlfile.\n");
 }
 
 # send to output
