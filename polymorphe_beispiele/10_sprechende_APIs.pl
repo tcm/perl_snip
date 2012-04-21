abf097bd0e503a707d5a4f2ad05a6b3ff13a3b66
Juergen Bachsteffel <juergen@pass-ag.com>
2012-04-21 08:36:48 +0200

    Noch ein Beispiel.

---
+++ polymorphe_beispiele/10_sprechende_APIs.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w 
+# Listing 10 
+# aus Toolbox 5/2009
+#
+use strict;
+use warnings;
+use feature ':5.10';
+
+my $object = Toolbox->new({ HEFT => 5,
+                            SEITEN => 100,
+                            PREIS => 11.90,
+                            TITEL => 'Das Werkzeug...',
+                            LEVEL => 'Wizard'});
+
+package Toolbox;
+
+# Constructor
+sub new 
+{
+  my $class = shift; # Klassenname via Pfeilnotation
+  my $self = shift; # Referenz auf anonymen Hash
+
+  foreach my $param(keys %$self)
+  {
+     # Syntaktisches Salz
+     print "$param is set to: " . $$self{$param} . "\n";
+
+     # Syntaktischer Zucker
+     say "$param is set to: " . $self->{$param};
+  } 
+
+  bless($self, $class);
+  # Markierung der Referenz mit dem Klassennamen
+  return($self);
+}
