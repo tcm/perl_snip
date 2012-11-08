03810883d7d59a5d056823e48b9601bdcd8144bc
Juergen Bachsteffel <juergen@pass-ag.com>
2012-11-08 17:13:01 +0100

    Parameterübergabe.

--- misc/identify_files.pl
+++ misc/identify_files.pl
@@ -5,15 +5,13 @@
 use strict;
 use warnings;
 
-#use Cwd;
-#use File::Copy;
+use File::Copy;
 use File::Find;
 use POSIX qw( strftime );
 #use Devel::Size qw(size total_size);
-#use File::Path;
-#use File::Spec::Functions;
-#use File::Basename;
-#use File::Spec;
+use File::Path qw(make_path);
+use Path::Class;
+use Getopt::Std;
 #use Data::Dumper;
 use feature qw/switch/; 
 
@@ -41,36 +39,67 @@ my $num = 0;              # momentan höchste Endung.
 
 my $key;
 my $value;
-my $dest_path = "d:\\archiv\\"; # Zielpfad zum verschieben der Dateien.
-my $n;                          # Anzahl der Schlüssel im Hash.
+my $source_path;          # Quellpfad zum verschieben der Dateien.
+my $dest_path;            # Zielpfad zum verschieben der Dateien.
+my $n;                    # Anzahl der Schlüssel im Hash.
 my $timestamp;
+my $version="0.9";
+my %options;
 
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_1: Find candidates.\n";
-find(\&rekur, "d:\\proe_creo\\627\\");
-#find(\&rekur, "d:\\proe_creo\\627002");
+
+getopts('i:o:v',\%options);
+
+if(defined $options{v})
+{	
+print "\n---------------\n";
+print "Version: $version\n";
+print "---------------\n\n";
+}
+
+if(!defined $options{i})
+{	
+die "Quellverzeichniss (option -i) muss angegeben werden.\n";
+}
+
+if(!defined $options{o})
+{	
+die "Backupverzeichniss (option -o) muss angegeben werden.\n";
+}
+
+
+$source_path=file($options{i});
+$dest_path=file($options{o});
+
+
+#print "Quellpfad: $source_path\n";
+#print "Zielpfad: $dest_path\n";
+
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_1: Find candidates.\n";
+find(\&rekur, $source_path);
 
  
 ###########################
 # I. Anzahl der Dateien
-# pro Prefix bestimmen
+# pro Prefix bestimmen.
 ###########################
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_2: Gernerate Hash \%file_anz.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_2: Gernerate Hash \%file_anz.\n";
 foreach my $matched_file (@candidates)
 {
    my $datei = substr($matched_file, 0, rindex($matched_file, "."));
    $file_anz{$datei}++; # Anzahl der Files pro Prefix erhöhen.
+   #print "$matched_file\n";
 }
 
-$n = keys %file_anz; print "\%file_anz: $n\n";
+#$n = keys %file_anz; print "\%file_anz: $n\n";
 
 ##################################
 # II. Array nur mit den
 # relevanten Kandidaten erzeugen.
 ##################################
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_3: Create Array \@final were COUNT > 1.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_3: Create Array \@final were COUNT > 1.\n";
 
 foreach my $matched_file (@candidates)
 {
@@ -81,6 +110,8 @@ foreach my $matched_file (@candidates)
 @candidates = (); # Nicht benötige Datenstrukturen 
 %file_anz = ();   # löschen.
 
+$n = @final; print "$timestamp --  Anzahl: $n\n";
+
 ###############################
 # IV. Hash mit der höchsten
 # Endung pro Prefix erstellen.
@@ -88,8 +119,8 @@ foreach my $matched_file (@candidates)
 # Hash mit der zweithöchsten
 # Endung pro Prefix erstellen.
 ###############################
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_4: Create Hashes \%suff_max \%suff_max_minus_one.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_4: Create Hashes \%suff_max \%suff_max_minus_one.\n";
 foreach my $matched_file (@final)
 {
    $num = substr($matched_file, rindex($matched_file, ".")+1, );
@@ -110,13 +141,14 @@ foreach my $matched_file (@final)
 # V. Version-Pattern 
 # erzeugen.
 ###########################
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_5: Generate Version-Pattern.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_5: Generate Version-Pattern.\n";
 
-print "Process: Shorter Hash MAX.\n";
+print "$timestamp --  Process: Shorter Hash MAX.\n";
 construct_pattern(\%suff_max, \%version_pattern);
 
-print "Process: Shorter Hash MAX_MINUS_ONE.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  Process: Shorter Hash MAX_MINUS_ONE.\n";
 construct_pattern(\%suff_max_minus_one, \%version_pattern);
 
 
@@ -124,12 +156,27 @@ construct_pattern(\%suff_max_minus_one, \%version_pattern);
 # VI. Verschieben der  
 # Kandidaten.
 ###########################
-$timestamp = strftime '%d/%m/%Y %H:%M:%S', localtime;
-print "[$timestamp] PASS_6: Move Files to Archiv.\n";
+$timestamp = strftime '%d-%m-%Y %H:%M:%S', localtime;
+print "$timestamp --  PASS_6: Copy or Move Files to Archiv.\n\n";
 while ( ($key,$value) = each %version_pattern ) 
 {
-   if ($value eq "CW" ) { print "move $key.$suff_max{$key}\n" };
+   if ($value eq "CW" ) 
+   {
+   my $qfile =  file($key.".".$suff_max{$key});  # File-Objekt erzeugen (Filename + höchste Endung).
+   my $qdir  = substr($qfile->dir,3, );          # Den Basis-Pfad extrahieren 
+                                                 # und den Laufwerksbuchstaben abtrennen.
+
+   my $dest_path_new = file ($dest_path, $qdir); # Den neuen Zielpfad zusammensetzen:
+                                                 # Zielpfad + Verzeichnisteil des Quell-Files.
+
+   make_path("$dest_path_new");
+   print "Copy: ";  copy( $qfile, $dest_path_new) or die "File-Operation failed: $!";
+   #print "Move: "; move( $qfile, $dest_path_new) or die "File-Operation failed: $!";
+   print "$qfile\n";
+   }
+
 }
+print "\n\nZiel: $dest_path\n";
 
 
 ##################
@@ -142,7 +189,7 @@ sub rekur
    foreach my $rule (@crit)
    {	     
       # Kandidaten zwischenspeichern.
-      push(@candidates, grep{/$rule/} $File::Find::name); 
+      push(@candidates, grep{/$rule/} file($File::Find::name)); 
    }
 }
 
