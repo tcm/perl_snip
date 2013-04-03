a556f1ce61ba0bf7781f3c65e90e97da734d1ea0
Juergen Bachsteffel <juergen@pass-ag.com>
2013-04-03 07:28:09 +0200

    Neuauflage-2.

--- misc/netping.pl
+++ misc/netping.pl
@@ -8,13 +8,10 @@ use Net::Ping::External qw(ping);
 use Net::SMTP;
 use POSIX qw(strftime); 
 
-my @check_switch = qw(sw4208-1 sw2524-1 sw2524-3 sw2524-4 sw2524-5 sw2524-6 sw2524-7 sw2524-8 sw2524-9);
+my @check_switch = qw(swxxxxx-1  swxxxx-3 );
+);
 
-my @check_host = qw(vm-w2003-1 vm-w2003-2 vm-w2003-3 vm-xp-1 vm-sles10-1 nt1 vm-w2000srv-1 nt4 nt6 unix4
-                  vm-sles11-1 unix10 firewall ntws1 bildmaster1 bildmaster2 faxserver mail vm-w2003-4 
-                  vm-w2008-1 vm-w2008-2 vm-w2008-3 nt2 vm-debian3-0-1 vm-suse10-3-3 nas-2 nas-5);
-
-my @check_ap = qw(ap4000m-1 ap4000m-2);
+my @check_ap = qw(ap-1 ap-2);
 
 my @hosts = (@check_switch , @check_host , @check_ap );
 
