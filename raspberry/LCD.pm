965cfe39fd8d82c7a1c319ecde8e3b1330e0c923
Juergen Bachsteffel <juergen@pass-ag.com>
2019-04-15 18:16:03 +0200

    Kosmetik.

--- raspberry/LCD.pm
+++ raspberry/LCD.pm
@@ -16,17 +16,18 @@ our @EXPORT = qw(deleteDigit deleteAllDigits switchDigit showDigit showTwoDigits
 our @EXPORT_OK = qw(deleteDigit deleteAllDigits switchDigit showDigit showTwoDigits switchSegment);
 our %EXPORT_TAGS = ( Functions => [ qw(deleteDigit deleteAllDigits switchDigit showDigit showTwoDigits switchSegment) ] );
 
+# Pin-Nummern der einzelnen Segmente
 my %segment = ( "a" => 7,
-				"b" => 8,
-				"d" => 23,
+		"b" => 8,
+		"d" => 23,
                 "c" => 24,
-				"e" => 18,
+		"e" => 18,
                 "f" => 12,
                 "g" => 16,
                 "dp" => 25,
                 "01" => 14,
                 "10" => 15 );
-
+# Segmente, die für eine Ziffer leuchten müssen
 my %digit = ( 0 => "abcdef",
               1 => "bc",
               2 => "abged",
@@ -41,7 +42,7 @@ my %digit = ( 0 => "abcdef",
 
 # Segment an- oder abschalten.
 sub switchSegment {
-   my $segchar = shift;  # Bezeichnung des Segments
+   my $segchar = shift;    # Bezeichnung des Segments (a-f, 01, 10)
    my $state = shift;	   # Aus oder An (0/1)
    
 
@@ -56,12 +57,13 @@ sub switchSegment {
    }
 }
 
-# Digit 01 oder 10 aktivieren.
+# Digit 01 oder 10 aktivieren,
+# sprich die Dezimalstelle umschalten (01 Einer, 10, Zehner)
 sub switchDigit {
    my $state = shift;	   # Digit 01 oder 10 
    
-   if ($state eq "01") {
-	my $pin = RPi::Pin->new($segment{"01"});
+   if ($state eq "01") { # Wollen wir die Einer-Stelle?
+	my $pin = RPi::Pin->new($segment{"01"}); 
    	$pin->mode(OUTPUT);
    	$pin->write(HIGH);
    	
@@ -69,7 +71,7 @@ sub switchDigit {
    	$pin->mode(OUTPUT);
    	$pin->write(LOW);
 
-   } elsif ($state eq "10"  ) {
+   } elsif ($state eq "10"  ) { # oder die Zehner-Stelle?
 	my $pin = RPi::Pin->new($segment{"10"});
    	$pin->mode(OUTPUT);
    	$pin->write(HIGH);
@@ -80,10 +82,10 @@ sub switchDigit {
    }
 }
 
-# Ziffer löschen.
+# Löscht die Anzeige (1 Ziffer).
 sub deleteDigit{
-   
-   foreach my $char (split //, $digit{8}) {
+ 
+   foreach my $char (split //, $digit{8}) { # Alle 8 Segmente ausschalten.
 	   switchSegment($char,0);
    }
 }
@@ -99,7 +101,7 @@ sub deleteAllDigits{
 
 # Ziffer anzeigen.
 sub showDigit{
-	my $digitnum = shift; # Eine einstellige Ziffer
+   my $digitnum = shift; # Eine einstellige Ziffer (0-9)
    
    foreach my $char (split //, $digit{$digitnum}) {
 	   switchSegment($char,1);
@@ -108,20 +110,20 @@ sub showDigit{
 
 # 2 Ziffern im Wechsel anzeigen.
 sub showTwoDigits{
-	my $num = shift; # Eine zweistellige Ziffer
+    my $num = shift; # Eine zweistellige Ziffer
 	
-	my $digit01 = substr($num,1);
-    my $digit10 = substr($num,0,1);
+    my $digit01 = substr($num,1);   # Einer extrahieren.
+    my $digit10 = substr($num,0,1); # Zehner extrahieren.
 
-	foreach my $n (1 .. 1000 ) {
+    foreach my $n (1 .. 1000 ) {
 	
-    switchDigit("10");
-    showDigit($digit10);
-    deleteDigit;
+       switchDigit("10");
+       showDigit($digit10);
+       deleteDigit;
      
-	switchDigit("01");
-	showDigit($digit01);
-    deleteDigit;
+       switchDigit("01");
+       showDigit($digit01);
+       deleteDigit;
  }
 }
 
